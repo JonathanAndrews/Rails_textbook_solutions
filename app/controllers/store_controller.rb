07f4b1063e6f89ae321741336929ddf1d210e8b6
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-02-04 09:21:32 +0000

    added a button to add items to cart and a visits counter to catalog page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:visits].nil?
+      session[:visits] = 1
+    else
+      session[:visits] += 1
+    end
+    @visits = session[:visits]
   end
 end
