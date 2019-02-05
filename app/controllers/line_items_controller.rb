ae3583cc9af644ffa592caed7d9d6d014bf3beb5
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-02-05 09:22:12 +0000

    customer cart now shows the quantity of an item in the cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,7 +27,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
     session[:visits] = 0
 
     respond_to do |format|
