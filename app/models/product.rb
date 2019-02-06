0a2ce9e3ccc849a66655578a00369a0e0d441dd4
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-02-06 08:17:44 +0000

    Handling Errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,7 +14,7 @@ class Product < ApplicationRecord
   private
     # ensure that there are no line items referencing this product
     def ensure_not_referenced_by_any_line_item
-      unless ling_items.empty?
+      unless line_items.empty?
         errors.add(:base, 'Line Items present')
         throw :abort
       end
