e9878ca680c14615fc092f7428db5ef2c10e171c
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-02-07 09:08:04 +0000

    testing cart can add unique products and duplicate products

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cart accepts unique line items" do
+    cart = Cart.new
+    cart.add_product(products(:one)).save!
+    cart.add_product(products(:two)).save!
+
+    assert_equal 2, cart.line_items.size
+    assert_equal products(:one).price + products(:two).price, cart.total_price
+  end
+
+  test "cart accepts duplicate line items" do
+    cart = Cart.create
+    book_one = products(:one)
+    cart.add_product(book_one).save!
+    cart.add_product(book_one).save!
+
+    assert_equal 2*book_one.price, cart.total_price
+    assert_equal 1, cart.line_items.size
+    assert_equal 2, cart.line_items[0].quantity
+  end
+
 end
