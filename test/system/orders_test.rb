b6b7cd946e879b6e1f92cc8b8be1060c35fdb28d
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-02-14 13:49:49 +0000

    Testing Our JavaScript Functionality

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -46,4 +46,22 @@ class OrdersTest < ApplicationSystemTestCase
 
     assert_text "Order was successfully destroyed"
   end
+
+  test "check routing number" do
+    visit store_index_url
+
+    first('.catalog li').click_on 'Add to Cart'
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector "#order_routing_number" 
+
+    select 'Check', from: 'pay_type'
+
+    assert_selector "#order_routing_number"
+  end
 end
