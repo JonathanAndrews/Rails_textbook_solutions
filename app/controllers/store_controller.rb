72509f044d12c68f8ce5fc90cb156d28a7bf6168
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-02-15 17:57:36 +0000

    Adding a Locale Switcher

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,12 +3,16 @@ class StoreController < ApplicationController
   skip_before_action :authorize
   before_action :set_cart
   def index
-    @products = Product.order(:title)
-    if session[:visits].nil?
-      session[:visits] = 1
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
     else
-      session[:visits] += 1
+      @products = Product.order(:title)
+      if session[:visits].nil?
+        session[:visits] = 1
+      else
+        session[:visits] += 1
+      end
+      @visits = session[:visits]
     end
-    @visits = session[:visits]
   end
 end
