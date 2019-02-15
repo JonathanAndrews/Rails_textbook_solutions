aea9207a940d4431fcd4c6cb73f865eae23cfe52
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2019-02-15 12:21:44 +0000

    Authenticating Users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  validates :name, presence: true, uniqueness: true
-  has_secure_password
+  # test "the truth" do
+  #   assert true
+  # end
 end
